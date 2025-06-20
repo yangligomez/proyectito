5daf15f22bbe0ad54e2f284e4b572d389a0fd47f
yangligomez <yang.gomez@correounivalle.edu.co>
2025-06-20 01:29:25 -0500

    cambios de main

--- Main.py
+++ Main.py
@@ -1,30 +1,7 @@
-from ventanaPrincipal import VentanaPrincipal
 import tkinter as tk
-from recepcionista_funciones import Recepcionista
-from conexion import obtener_conexion  # si ya tienes un archivo para conectar a la BD
-
-conn = obtener_conexion()
-
-recep = Recepcionista("111", "Laura", "Suárez", "3001234567", "laura@correo.com")
-recep.inscribir_estudiante("123456789", 1, conn)  # Usa un curso real de tu BD
-# Crear una instancia del recepcionista (puede ser temporal para pruebas)
-recep = Recepcionista("111", "Laura", "Suárez", "3001234567", "laura@correo.com")
-
-# Probar inscripción
-conn = obtener_conexion()
-recep.inscribir_estudiante("123456789", 1, conn)
-
-# Probar cancelación
-recep.cancelar_inscripcion("123456789", 1, conn)
-
-from recepcionista_funciones import Recepcionista
-from conexion import obtener_conexion
-
-conn = obtener_conexion()
-recep = Recepcionista("111", "Laura", "Suárez", "3001234567", "laura@correo.com")
-recep.inscribir_estudiante("123456789", 1, conn)  # Usa cédula e ID de curso reales
+from ventanaPrincipal import VentanaPrincipal
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = VentanaPrincipal(root, usar_root=True)  # Nuevo argumento
+    app = VentanaPrincipal(root, usar_root=True)
     root.mainloop()
