fe7f2af38cc1502a8da7c87ae74b99d1069f4571
yangligomez <yang.gomez@correounivalle.edu.co>
2025-06-20 02:03:00 -0500

    actualizacion de main

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from ventanaPrincipal import VentanaPrincipal
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = VentanaPrincipal(root, usar_root=True)
-    root.mainloop()
+class Main:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.app = VentanaPrincipal(self.root, usar_root=True)
+        self.root.mainloop()
+
+
+Main()
